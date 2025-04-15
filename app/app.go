cf3c4c5c7e6d8618562fae37dabfb3f0b5d163ee
Tony Chen <codchen03@gmail.com>
2025-04-15 14:48:13 +0800

    Add solo precompile for claim function

--- app/app.go
+++ app/app.go
@@ -731,6 +731,7 @@ func New(
 	if enableCustomEVMPrecompiles {
 		customPrecompiles := precompiles.GetCustomPrecompiles(
 			LatestUpgrade,
+			encodingConfig.TxConfig,
 			&app.EvmKeeper,
 			app.BankKeeper,
 			bankkeeper.NewMsgServerImpl(app.BankKeeper),
